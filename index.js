9d127847fceb607297fc6eb3510b49849690727f
Huw Jones <huwjones025@gmail.com>
2019-09-24 15:08:38 +0100

    sequenceJobs passes empty string test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const sequenceJobs = () => {
+/*
+Imagine we have a list of jobs, each represented by a character. Because certain jobs must be done before others, a job may have a
+dependency on another job. For example, a may depend on b, meaning the final sequence of jobs should place b before a. If a has no
+dependency, the position of a in the final sequence does not matter.
+*/
 
+const sequenceJobs = () => {
+  return '';
 }
 
 module.exports = sequenceJobs;
--- index.spec.js
+++ index.spec.js
@@ -1,3 +1,11 @@
 const {expect} = require('chai');
 const sequenceJobs = require('./index');
 
+describe('sequenceJobs', () => {
+  it('should return an empty string when passed an empty string', () => {
+    const input = '';
+    const result = sequenceJobs(input);
+    const expected = '';
+    expect(result).to.equal(expected);
+  });
+});
